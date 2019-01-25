8df32dd6a7525b2f5c5ccc9fcf4fbc013658aee7
Masami Yonehara <zeitdiebe@gmail.com>
2019-01-25 17:12:25 +0900

    Simplify my code. Get rid of example codes

--- src/containers/AsyncApp.tsx
+++ src/containers/AsyncApp.tsx
@@ -1,100 +1,23 @@
 import * as React from 'react'
 import { connect } from 'react-redux'
-import {
-  selectSubreddit,
-  fetchPostsIfNeeded,
-  invalidateSubreddit,
-} from '../actions'
-import Picker from '../components/Picker'
-import Posts from '../components/Posts'
+import List from '../components/List'
 
 export interface AsyncAppProps {
   dispatch: any
-  selectedSubreddit: any
-  posts: any
-  isFetching: any
-  lastUpdated: any
 }
 
 class AsyncApp extends React.Component<AsyncAppProps, {}> {
-  constructor(props: any) {
-    super(props)
-    this.handleChange = this.handleChange.bind(this)
-    this.handleRefreshClick = this.handleRefreshClick.bind(this)
-  }
-
-  componentDidMount() {
-    const { dispatch, selectedSubreddit } = this.props
-    dispatch(fetchPostsIfNeeded(selectedSubreddit))
-  }
-
-  componentDidUpdate(prevProps: any) {
-    if (this.props.selectedSubreddit !== prevProps.selectedSubreddit) {
-      const { dispatch, selectedSubreddit } = this.props
-      dispatch(fetchPostsIfNeeded(selectedSubreddit))
-    }
-  }
-
-  handleChange(nextSubreddit: any) {
-    this.props.dispatch(selectSubreddit(nextSubreddit))
-    this.props.dispatch(fetchPostsIfNeeded(nextSubreddit))
-  }
-
-  handleRefreshClick(e: any) {
-    e.preventDefault()
-
-    const { dispatch, selectedSubreddit } = this.props
-    dispatch(invalidateSubreddit(selectedSubreddit))
-    dispatch(fetchPostsIfNeeded(selectedSubreddit))
-  }
-
   render() {
-    const { selectedSubreddit, posts, isFetching, lastUpdated } = this.props
     return (
       <div>
-        <Picker
-          value={selectedSubreddit}
-          onChange={this.handleChange}
-          options={['reactjs', 'frontend']}
-        />
-        <p>
-          {lastUpdated && (
-            <span>
-              Last updated at {new Date(lastUpdated).toLocaleTimeString()}.{' '}
-            </span>
-          )}
-          {!isFetching && (
-            <button onClick={this.handleRefreshClick}>Refresh</button>
-          )}
-        </p>
-        {isFetching && posts.length === 0 && <h2>Loading...</h2>}
-        {!isFetching && posts.length === 0 && <h2>Empty.</h2>}
-        {posts.length > 0 && (
-          <div style={{ opacity: isFetching ? 0.5 : 1 }}>
-            <Posts posts={posts} />
-          </div>
-        )}
+        <List list={[]} />
       </div>
     )
   }
 }
 
-function mapStateToProps(state: any) {
-  const { selectedSubreddit, postsBySubreddit } = state
-  const { isFetching, lastUpdated, items: posts } = postsBySubreddit[
-    selectedSubreddit
-  ] || {
-    lastUpdated: 0,
-    isFetching: true,
-    items: [],
-  }
-
-  return {
-    selectedSubreddit,
-    posts,
-    isFetching,
-    lastUpdated,
-  }
+function mapStateToProps() {
+  return {}
 }
 
 export default connect(mapStateToProps)(AsyncApp)
